cf9647300e2cab8e19d6e6a9d6d8b5f3f8b7b77b
Nedelcho <160885648+NedelchoPopov@users.noreply.github.com>
2025-06-25 21:49:33 +0300

    Add project files.

---
+++ NaturaStore.Data/NaturaStoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NaturaStore.Data
+{
+    public class NaturaStoreDbContext : IdentityDbContext
+    {
+        public NaturaStoreDbContext(DbContextOptions<NaturaStoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
